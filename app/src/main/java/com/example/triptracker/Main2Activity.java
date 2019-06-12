63c2d47a0745179fc4802cf9b2cce06735126d3d
TimHoreweg <timhoreweg@hotmail.com>
2019-06-12 14:06:16 +0200

    Revert "ergethrtyu"
    This reverts commit 46a697e99684ded39f371692603dff209c15704f.

--- app/src/main/java/com/example/triptracker/Main2Activity.java
+++ app/src/main/java/com/example/triptracker/Main2Activity.java
@@ -58,11 +58,8 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 <<<<<<< HEAD
-<<<<<<< HEAD
         mSearchText = findViewById(R.id.textInputLayout2);
 =======
-=======
->>>>>>> parent of f0eec3c... Merge branch 'Manuel' of https://github.com/XanYder/TripTracker2 into Manuel
         mSearchText = findViewById(R.id.location);
 >>>>>>> parent of f0eec3c... Merge branch 'Manuel' of https://github.com/XanYder/TripTracker2 into Manuel
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
