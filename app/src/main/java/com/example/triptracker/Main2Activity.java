68d1d0525d7c7d704d5df4991329969bd95b3ba4
manuelkoegler <mekoegler@hotmail.com>
2019-06-12 12:03:59 +0200

    Revert "Revert "WIP add current location automatically""
    This reverts commit cc6df4b1aa381b36a89ae8badfe8891750854c1a.

--- app/src/main/java/com/example/triptracker/Main2Activity.java
+++ app/src/main/java/com/example/triptracker/Main2Activity.java
@@ -61,18 +61,14 @@ public class Main2Activity extends AppCompatActivity {
         PlacesClient placesClient = Places.createClient(this);
         mSearchText = findViewById(R.id.location);
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        /*fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(Location location) {
                 // Got last known location. In some rare situations this can be null.
-                Toast.makeText(Main2Activity.this, location.toString(),
+                Toast.makeText(Main2Activity.this, Double.toString(location.getLatitude()),
                         Toast.LENGTH_LONG).show();
-                if (location != null) {
-                    Toast.makeText(Main2Activity.this, "location doesn't equal null",
-                            Toast.LENGTH_LONG).show();
-                }
             }
-        });*/
+        });
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
